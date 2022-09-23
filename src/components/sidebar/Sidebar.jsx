18a75eb973da97fc0fa80883224aee74fad7799e
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-09-24 00:06:01 +0900

    サイドバーを作成しよう

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/*ツイッターアイコン*/}
+      {/*SidebarOption*/}
+      {/*SidebarOption*/}
+      {/*SidebarOption*/}
+      {/*SidebarOption*/}
+      {/*SidebarOption*/}
+      {/*ツイッターボタン*/}
+    </div>
+  );
+};
+
+export default Sidebar;
