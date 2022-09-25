1bb8318d43d49521c54a2ec1557c7b0ddd1360ba
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-09-26 00:04:17 +0900

    Material Icon追加

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { SidebarOption } from "./SidebarOption";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
       {/*ツイッターアイコン*/}
       <TwitterIcon className="sidebar__twitterIcon" />
       <SidebarOption text="ホーム" Icon={HomeIcon} />
-      <SidebarOption text="話題を検索" Icon={HomeIcon} />
-      <SidebarOption text="通知" Icon={HomeIcon} />
-      <SidebarOption text="メッセージ" Icon={HomeIcon} />
-      <SidebarOption text="ブックマーク" Icon={HomeIcon} />
-      <SidebarOption text="リスト" Icon={HomeIcon} />
-      <SidebarOption text="プロフィール" Icon={HomeIcon} />
-      <SidebarOption text="もっと見る" Icon={HomeIcon} />
+      <SidebarOption text="話題を検索" Icon={SearchIcon} />
+      <SidebarOption text="通知" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="メッセージ" Icon={MailOutlineIcon} />
+      <SidebarOption text="ブックマーク" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="リスト" Icon={ListAltIcon} />
+      <SidebarOption text="プロフィール" Icon={PermIdentityIcon} />
+      <SidebarOption text="もっと見る" Icon={MoreHorizIcon} />
       {/*ツイッターボタン*/}
     </div>
   );
