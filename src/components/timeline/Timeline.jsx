27178f7d2bc2a31c1c1abffe7c84f41aa9348a41
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-09-27 09:50:24 +0900

    タイムラインを作成しよう

---
+++ src/components/timeline/Timeline.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Timeline = () => {
+  return (
+    <div className="timeline">
+      {/* Header */}
+      <div className="timeline__header">
+        <h2>ホーム</h2>
+      </div>
+      {/* Tweetbox */}
+      {/* Post */}
+    </div>
+  );
+};
+
+export default Timeline;
