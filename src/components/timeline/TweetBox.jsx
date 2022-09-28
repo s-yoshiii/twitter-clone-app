4137c34452ba290787393bcdb3e73ac117ab11fc
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-09-29 00:36:00 +0900

    TweetBox.cssでスタイリングを適用しよう

--- src/components/timeline/TweetBox.jsx
+++ src/components/timeline/TweetBox.jsx
@@ -1,18 +1,24 @@
 import { Avatar, Button } from "@mui/material";
 import React from "react";
-
+import "./TweetBox.scss";
 const TweetBox = () => {
   return (
     <div className="tweetBox">
       <form>
-        <Avatar />
-        <input type="text" placeholder="いまどうしてる？" />
+        <div className="tweetBox__input">
+          <Avatar />
+          <input type="text" placeholder="いまどうしてる？" />
+        </div>
         <input
           className="tweetBox__imageInput"
           placeholder="画像のURLを入力してください"
           type="text"
         />
-        <Button type="submit" className="tweetBox__tweetButton">
+        <Button
+          type="submit"
+          className="tweetBox__tweetButton"
+          sx={{ ml: "auto" }}
+        >
           ツイートする
         </Button>
       </form>
