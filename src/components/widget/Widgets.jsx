a084b6d85ec00d337eeb5072711014b1fe0ca4ce
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-01 21:15:48 +0900

    ツイッタータイムライン貼り付けのライブラリを導入してみよう

--- src/components/widget/Widgets.jsx
+++ src/components/widget/Widgets.jsx
@@ -1,5 +1,10 @@
 import { Search } from "@mui/icons-material";
 import React from "react";
+import {
+  TwitterTweetEmbed,
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+} from "react-twitter-embed";
 
 export const Widgets = () => {
   return (
@@ -10,6 +15,16 @@ export const Widgets = () => {
       </div>
       <div className="widgets__widgetContainer">
         <h2>今どうしている？</h2>
+        <TwitterTweetEmbed tweetId={"1576134851090071552"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="yoshiii_s"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://twitter.com/yoshiii_s"}
+          options={{ text: "#react.js勉強中", via: "yoshiii_s" }}
+        />
       </div>
     </div>
   );
