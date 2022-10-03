72702c76a3918794132265782e64270f518cf76e
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-03 23:25:54 +0900

    Firebaseの初期化設定

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
 import Timeline from "./components/timeline/Timeline";
 import { Widgets } from "./components/widget/Widgets";
-
 function App() {
   return (
     <div className="app">
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "twitter-clone-app-9a224.firebaseapp.com",
+  projectId: "twitter-clone-app-9a224",
+  storageBucket: "twitter-clone-app-9a224.appspot.com",
+  messagingSenderId: "986257707172",
+  appId: process.env.APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
