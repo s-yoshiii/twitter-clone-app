2d77f0280da94c76e65ed7b7af1e61c274081901
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-05 00:08:48 +0900

    取り出したデータをPost.jsに渡して出力してみよう

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Timeline.scss";
 import TweetBox from "./TweetBox";
 import db from "../../firebase";
 import { collection, getDocs } from "firebase/firestore";
 const Timeline = () => {
+  const [posts, setPosts] = useState([]);
   const postData = collection(db, "posts");
   getDocs(postData).then((querySnapshot) => {
-    console.log(querySnapshot.docs.map((doc) => doc.data()));
+    setPosts(querySnapshot.docs.map((doc) => doc.data()));
   });
   return (
     <div className="timeline">
@@ -16,14 +17,16 @@ const Timeline = () => {
         <h2>ホーム</h2>
       </div>
       <TweetBox />
-      <Post
-        displayName="プログラミングチュートリアル"
-        username="yoshiii_s"
-        verified={true}
-        text="はじめてのツイート"
-        avatar="https://pbs.twimg.com/profile_images/1504842282121392132/1JiRCd_m_400x400.jpg"
-        image="https:/source.unsplash.com/random"
-      />
+      {posts.map((post) => (
+        <Post
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+        />
+      ))}
     </div>
   );
 };
