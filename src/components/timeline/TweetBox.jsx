655d1e6f227c498ce5f2f419c1c7fa82b9a7d687
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-05 09:52:02 +0900

    ツイート投稿できるようにしよう：その１

--- src/components/timeline/TweetBox.jsx
+++ src/components/timeline/TweetBox.jsx
@@ -1,23 +1,35 @@
 import { Avatar, Button } from "@mui/material";
 import React from "react";
+import { useState } from "react";
 import "./TweetBox.scss";
 const TweetBox = () => {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+  const sendTweet = (e) => {
+    // firebaseのデータベースにデータを追加する
+  };
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar />
-          <input type="text" placeholder="いまどうしてる？" />
+          <input
+            type="text"
+            placeholder="いまどうしてる？"
+            onChange={(e) => setTweetMessage(e.target.value)}
+          />
         </div>
         <input
           className="tweetBox__imageInput"
           placeholder="画像のURLを入力してください"
           type="text"
+          onChange={(e) => setTweetImage(e.target.value)}
         />
         <Button
           type="submit"
           className="tweetBox__tweetButton"
           sx={{ ml: "auto" }}
+          onClick={sendTweet}
         >
           ツイートする
         </Button>
